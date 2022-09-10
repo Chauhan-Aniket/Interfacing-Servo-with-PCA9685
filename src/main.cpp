e8877c809c7be48b0571ac254019b884e653d3e1
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-09-10 14:07:37 +0530

    create: required variable & function

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,43 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+// Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+// Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+#define SERVOMIN 150  // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600  // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+int servonum = 0;
+int totalServo = 16;
+
 void setup()
 {
-  // put your setup code here, to run once:
+  pwm.begin();
+  Serial.begin(115200);
+  if (!Serial)
+    return;
+
+  Serial.println("Servo test!");
+
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ); // Analog servos run at ~50 Hz updates
+
+  delay(10);
+}
+
+int angleToPulse(int ang)
+{
+  int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX); // map angle of 0 to 180 to Servo min and Servo max
+  Serial.print("Angle: ");
+  Serial.print(ang);
+  Serial.print(" pulse: ");
+  Serial.println(pulse);
+  return pulse;
 }
 
 void loop()
